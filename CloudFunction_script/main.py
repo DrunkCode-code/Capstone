490cae1cb74f891c190601944d0cad6c0a69d56a
1808561041 - Made Dwiki Budi Laksana <55054907+DrunkCode-code@users.noreply.github.com>
2021-06-03 23:04:51 +0800

    update cloud function

--- CloudFunction_script/main.py
+++ CloudFunction_script/main.py
@@ -10,51 +10,57 @@ from google.protobuf.struct_pb2 import Value
 import requests
 import json
 import tensorflow as tf
+# lebar gambar
+lebar = 150
+# label prediksi
+label = ['Anggur','Brokoli','Gandum','Jagung','Kapas','Kubis','Padi','Rami','Tebu','Zaitun']
 
-IMG_WIDTH = 150
-COLUMNS = ['Anggur','Brokoli','Gandum','Jagung','Kapas','Kubis','Padi','Rami','Tebu','Zaitun']
-
-aip_client = aiplatform.gapic.PredictionServiceClient(client_options={
+# simpan endpoint
+ai_platform_url = aiplatform.gapic.PredictionServiceClient(client_options={
     'api_endpoint': 'us-central1-aiplatform.googleapis.com'
 })
-aip_endpoint_name = f'projects/{os.environ["GCP_PROJECT"]}/locations/us-central1/endpoints/{os.environ["ENDPOINT_ID"]}'
+aip_endpoint = f'projects/{os.environ["GCP_PROJECT"]}/locations/us-central1/endpoints/{os.environ["ENDPOINT_ID"]}'
 
 
 def get_prediction(instance):
-    logging.info('Sending prediction request to AI Platform ...')
+    logging.info('Sedang mengirim request prediksi ke AI Platform...')
+    # coba request ke model Vertex AI
     try:
-        pb_instance = json_format.ParseDict(instance, Value())
-        response = aip_client.predict(endpoint=aip_endpoint_name,
-                                      instances=[pb_instance])
+        json_instance = json_format.ParseDict(instance, Value())
+        response = ai_platform_url.predict(endpoint=aip_endpoint,
+                                      instances=[json_instance])
         return list(response.predictions[0])
+    # gagal request
     except Exception as err:
-        logging.error(f'Prediction request failed: {type(err)}: {err}')
+        logging.error(f'Prediksi gagal dengan keterangan: {type(err)}: {err}')
         return None
 
 
 def preprocess_image(image_url):
-    logging.info(f'Fetching image from URL: {image_url}')
+    logging.info(f'Fetching request gambar: {image_url}')
+    # coba request gambar
     try:
         image_response = requests.get(image_url)
         image_response.raise_for_status()
         assert image_response.headers.get('Content-Type') == 'image/jpeg'
+    # gagal fetching
     except (ConnectionError, requests.exceptions.RequestException,
             AssertionError):
-        logging.error(f'Error fetching image from URL: {image_url}')
+        logging.error(f'Error fetching gambar: {image_url}')
         return None
 
-    logging.info('Decoding and preprocessing image ...')
+    logging.info('Sedang decoding dan preprocessing gambar ...')
+    # resize gambar
     image = tf.io.decode_jpeg(image_response.content, channels=3)
-    image = tf.image.resize_with_pad(image, IMG_WIDTH, IMG_WIDTH)
+    image = tf.image.resize_with_pad(image, lebar, lebar)
     image = image / 255.
-    return image.numpy().tolist()  # Make it JSON-serializable
+    # membuat gambar menjadi json
+    return image.numpy().tolist()
 
 
 def predict(request):
-    # Set CORS headers for the preflight request
+    # HTTP Request method harus POST
     if request.method == 'OPTIONS':
-        # Allows POST requests from any origin with the Content-Type
-        # header and caches preflight response for an 3600s
         headers = {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'POST',
@@ -63,36 +69,39 @@ def predict(request):
         }
         return ('', 204, headers)
 
-    # Disallow non-POSTs
+    # Jika bukan post
     if request.method != 'POST':
-        return ('Not found', 404)
+        return ('Tidak ditemukan!', 404)
 
-    # Set CORS headers for the main request
+    # ubah nilai header ke main request
     headers = {'Access-Control-Allow-Origin': '*'}
 
+    # jika bukan json atau 'image_url' tidak ditemukan di json
     request_json = request.get_json(silent=True)
     if not request_json or not 'image_url' in request_json:
-        return ('Invalid request', 400, headers)
+        return ('Kesalahan Request', 400, headers)
 
+    # jika instance gagal melakukan preprocess
     instance = preprocess_image(request_json['image_url'])
     if not instance:
-        return ('Invalid request', 400, headers)
+        return ('Kesalahan Request', 400, headers)
 
-    raw_prediction = get_prediction(instance)
-    if not raw_prediction:
-        return ('Error getting prediction', 500, headers)
+    # lakukan prediksi, jika gagal maka error 500
+    coba_prediksi = get_prediction(instance)
+    if not coba_prediksi:
+        return ('Gagal memprediksi!', 500, headers)
 
-    probabilities = zip(COLUMNS, raw_prediction)
+    probabilities = zip(label, coba_prediksi)
     # max_probabilities = max(probabilities)
-    sorted_probabilities = sorted(probabilities,
-                                  key=itemgetter(1),
-                                  reverse=True)
+    sorted_probabilities = sorted(probabilities, key=itemgetter(1), reverse=True)
+    # ambil hanya key tertinggi
     max_probabilities = sorted_probabilities[0][0]
     respon_json = {
         "prediction": str(max_probabilities)
     }
     
-    logging.info('Prediction is %s', respon_json)
+    logging.info('Prediksinya adalah %s', respon_json)
     # return (jsonify(max_probabilities), 200, headers)
+    # kirim response dalam bentuk json
     return json.dumps(respon_json)
 
